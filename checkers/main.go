3c1869b9651d9945ec4b196565e5c55d51ebc69b
stevengu <stevengu@student.21-school.ru>
2024-10-10 00:22:24 +0300

    added checkers

---
+++ checkers/main.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func captureableWhite(i, j, n, m int, array [][]int) bool{
+	if i > 1 {
+		if j > 1 && array[i + 1][j - 1] == 2 && array[i - 2][j - 2] == 0{
+			return true
+		}
+		if j < m - 1 &&{
+
+		}
+	}
+}
